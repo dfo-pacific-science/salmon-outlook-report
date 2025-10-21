9d6462dc309d5a71246db1caa863df32d4252640
Stephen Finnis <stephenfinnis7@gmail.com>
2025-10-21 14:07:53 -0700

    add purrr package for excel files with multiple sheets

--- testingTables.R
+++ testingTables.R
@@ -46,3 +46,92 @@ ft = flextable(df_body) %>%
 
 # 7) Show the table
 ft
+
+#################################################################################
+
+
+
+
+
+####################################################################################
+
+
+library("purrr")
+
+dummyPath = "data/finnis_outlook_phase1_test_dummy_data_20250905.xlsx"
+
+# Locate the file by its relative path
+#file = doclib$get_item("General/Pacific Salmon Data Community of Practice/Projects & Task Teams/Reproducible Data Products/Salmon Outlook Reporting/Background Research/outlook_phase1_test_dummy_data_20250905.xlsx")
+
+
+# List all sheet names
+sheet_names = readxl::excel_sheets(dummyPath)
+
+# Read each sheet and name the list elements
+df_list = map(sheet_names, ~ read_excel(dummyPath, sheet = .x)) %>%
+  set_names(sheet_names)
+
+# Export each dataframe to the global environment
+list2env(df_list, envir = .GlobalEnv)
+
+# Columns we want to keep (now using parentrowid instead of uniquerowid)
+keep_cols_repeat = c(
+  "globalid", "uniquerowid",
+  "smu_area", "smu_species", "smu_name",
+  "outlook_narrative", "smu_outlook_assignment", "smu_prelim_forecast"
+)
+
+keep_cols_cu = c(
+  "cu_outlook_selection", "cu_outlook_assignment", "cu_prelim_forecast", "cu_count",
+  "parentrowid"
+)
+
+keep_cols_other = c(
+  "other_outlook_selection", "other_outlook_assignment", "other_prelim_forecast", "parentrowid"
+)
+
+Outlook_Repeat_Test = Outlook_Repeat_Test %>%
+  select(all_of(keep_cols_repeat))
+
+cu_outlook_records = cu_outlook_records %>%
+  select(all_of(keep_cols_cu))
+
+other_outlook_records = other_outlook_records %>%
+  select(all_of(keep_cols_other))
+
+
+
+# Step 2: Join smu_area, smu_species, smu_name from Outlook_Repeat_Test to cu_outlook_records
+cu_outlook_records_enriched = cu_outlook_records %>%
+  left_join(
+    Outlook_Repeat_Test %>%
+      select(uniquerowid, smu_area, smu_species, smu_name),
+    by = c("parentrowid" = "uniquerowid")
+  )
+
+# Step 2: Join smu_area, smu_species, smu_name from Outlook_Repeat_Test to cu_outlook_records
+other_outlook_records_enriched = other_outlook_records %>%
+  left_join(
+    Outlook_Repeat_Test %>%
+      select(uniquerowid, smu_area, smu_species, smu_name),
+    by = c("parentrowid" = "uniquerowid")
+  )
+
+
+#test = full_join(Outlook_Repeat_Test, cu_outlook_records_enriched, by = c("uniquerowid" = "parentrowid"))
+
+stacked_df = bind_rows(Outlook_Repeat_Test, cu_outlook_records_enriched, other_outlook_records_enriched)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
