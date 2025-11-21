ef2ebdf7da97c599414c60a2bd04a6cb3e101375
Stephen Finnis <stephenfinnis7@gmail.com>
2025-11-21 14:36:46 -0800

    more changes to slides

--- powerPointTables.R
+++ powerPointTables.R
@@ -4,29 +4,9 @@ library(flextable)
 library(dplyr)
 library(magrittr)
 
-# Slide dimensions
-slide_w <- 10
-slide_h <- 5.63
-
-# Margins
-left_margin  <- 0.5
-right_margin <- 0.5
-
-# Map size (unchanged)
-map_width    <- 7.95
-map_height   <- 5.14
-
-# Compute positions
-map_x <- slide_w - map_width - right_margin
-map_y <- (slide_h - map_height) / 2
-
-# Table size and position (fixed width)
-table_width  <- 2.0   # fixed width for 4 columns
-table_height <- map_height
-table_x <- left_margin
-table_y <- map_y
-
-# Styling function
+## ----------------------------
+## Helpers: styling functions
+## ----------------------------
 style_table <- function(ft) {
   ft <- bg(ft, part = "header", bg = "#3B4A5A") |>
     color(part = "header", color = "white") |>
@@ -42,7 +22,6 @@ style_table <- function(ft) {
   return(ft)
 }
 
-# Font size adjustment
 adjust_font <- function(ft) {
   n_rows <- nrow(ft$body$dataset)
   size <- if (n_rows <= 5) 18 else if (n_rows <= 10) 14 else 10
@@ -51,40 +30,98 @@ adjust_font <- function(ft) {
   return(ft)
 }
 
-# Example workflow
-ppt <- read_pptx("PracticePowerPoint.pptx")
-tables <- list(head(mtcars), head(iris))  # raw data frames
+## ----------------------------
+## Read pptx & pick a layout
+## ----------------------------
+ppt <- read_pptx("draftPrelimPres.pptx")
+
+# Get available layouts
+lsum <- layout_summary(ppt)
+layout_name <- lsum$layout[1]      # Use first available layout
+master_name <- lsum$master[1]
+
+## ----------------------------
+## Slide + object dimensions
+## ----------------------------
+sz <- slide_size(ppt)
+slide_w <- sz$width
+slide_h <- sz$height
+
+left_margin  <- 0.5
+right_margin <- 0.5
+
+map_width    <- 7.95
+map_height   <- 5.14
+table_width  <- 2.0
+table_height <- map_height
+
+map_y   <- (slide_h - map_height) / 2
+table_y <- map_y
+table_x <- left_margin
+map_x   <- slide_w - map_width - right_margin
+
+## ----------------------------
+## Example data
+## ----------------------------
+tables <- list(head(mtcars), head(iris))
 images <- c("data/maps/southCoastMap.png", "data/maps/fraserMap.png")
 
+## ----------------------------
+## Loop to add slides
+## ----------------------------
 for (i in seq_along(tables)) {
-  # Keep only first 4 columns
   df <- tables[[i]][, 1:4]
-  ft <- flextable(df)
-  ft <- adjust_font(ft)
+  ft <- flextable(df) |> adjust_font()
 
-  ppt <- on_slide(ppt, index = 4 + i)
+  ppt <- add_slide(ppt, layout = layout_name, master = master_name)
 
-  # Add table on the left
   ppt <- ph_with(
     ppt,
     value = ft,
-    location = ph_location(
-      left = table_x,
-      top  = table_y,
-      width = table_width,
-      height = table_height
-    )
+    location = ph_location(left = table_x, top = table_y, width = table_width, height = table_height)
   )
 
-  # Add map on the right
-  ppt <- ph_with(
-    ppt,
-    value = external_img(images[i], width = map_width, height = map_height),
-    location = ph_location(
-      left = map_x,
-      top  = map_y
+
+
+  if (file.exists(images[2])) {
+    ppt <- ph_with(
+      ppt,
+      value = external_img(images[2], width = map_width, height = map_height),
+      location = ph_location(left = map_x, top = map_y, width = map_width, height = map_height)
     )
-  )
+  } else {
+    warning(paste("Image not found:", images[i]))
+  }
+
+
+  ppt <- set_notes(ppt, value = "Hi Andrea! I'd like to stop working 4 the day blahhhhh",
+                   location = notes_location_type("body"))
+
+
+
+
 }
 
+## ----------------------------
+## Save updated presentation
+## ----------------------------
 print(ppt, target = "updated_presentation.pptx")
+
+
+
+
+#
+source("tableTest4.R")
+
+
+
+
+
+
+
+
+
+
+
+
+
