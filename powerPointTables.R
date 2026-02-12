bb67d99ae91957e7710974cc9217be8ae3912081
Stephen Finnis <stephenfinnis7@gmail.com>
2026-02-12 11:44:50 -0800

    better comments and update slide title to have area name

--- powerPointTables.R
+++ powerPointTables.R
@@ -1,94 +1,86 @@
 ################################################################################
 ### PRELIMINARY OUTLOOK PRESENTATION
-
-# This code puts together slides for the Preliminary Outlook Presentation
-# that is presented in mid-January
-
-# This code builds on a PowerPoint template, where the intro slides are already
-# created, and the overall formatting is already set
-
-# It creates tables from the Outlook data survey results, and adds tables for
-# each species and area as new slides
-# e.g., 1 table (slide) for Yukon Transboundary Sockeye, another slide for
-# Yukon Transboundary Chinook, etc.
-# Tables include the following information:
-# DFO area, Species, SMU name, Resolution, CU Name, Outlook, Forecast
-
-# Fraser and Interior Chinook/Sockeye need to be split out by SMU
-
-# Some manual re-formatting can be done after
-
-### PowerPoint notes from Dawn:
-# North Coast Coho: Order should be Nass, Skeena, Haida, Central
-# South Coast: order should be wcvi group, ecvi group for all south coast slides
-# Okanagan Sockeye and Chinook on same table.
-# 1) OKANAGAN SOCKEYE SALMON
-# 2) NO DESIGNATED SMU (CK-02) AND
-# 3) OKANAGAN CHINOOK SALMON (OKANAGAN_.X)
-
-# Remember to also add in the area names to the title
-
-# Yukon Transboundary Chinook order: alsek stikine taku at top, then yukon and porcupine for this area
-
-
-# Something about many lines in the table - wont fit
-# Fraser Map - added as placeholder
-# Maps are created by Chelsea or other GIS tech
+###
+### PURPOSE
+### -------
+### This script automates the creation of the Preliminary Outlook Presentation.
+### It uses a PowerPoint template and generates slides for each species and area
+### using survey results from the `tabPrep` dataset.
+###
+### SLIDE CONTENT
+### -------------
+### • Top-right banner: "OUTLOOKS – AREA NAME"
+### • Subtitle above table: "Area – Species"
+### • Table: SMU | Resolution | CU | Outlook | Forecast
+### • Map image (placeholder, if available)
+### • Narrative text added to slide notes
+###
+### SPECIAL CASE
+### ------------
+### • Fraser and Interior Chinook and Sockeye: one slide per SMU
+################################################################################
 
 
 ################################################################################
 ## READ IN THE DATA
-# Read in the source data from the code that generates the report
-# This code uses the data frame tabPrep
+################################################################################
+
+# Load the pre-processed survey data frame `tabPrep`
+# This contains the fields:
+# smu_area, smu_species, smu_name, Resolution, Name, Outlook, Forecast, Narrative
 source("statusTableWithOthers.R")
 
+
 ################################################################################
-## MAKE ADJUSTMENTS TO THE DATAFRAME
-# Select only relevant columns that will be displayed on each slide
-# Remember that Name is either the name of the SMU or CU
+## PREPARE THE DATA
+################################################################################
+
+# Keep only columns relevant for tables
+# smu_area & smu_species are kept for grouping only; they are not in the table
 tp_min = tabPrep %>%
   dplyr::select(smu_area, smu_species, smu_name, Resolution, Name, Outlook, Forecast)
 
-# Make some adjustments to the data frame
+# Adjust the data frame for table display
 tp_long = tp_min %>%
   dplyr::mutate(
-    # Rename smu_name to just "SMU"
-    SMU      = smu_name,
-    # Create a new CU column. e
-    # If data is provided for only an entire SMU (i.e., Resolution == SMU), fill in
-    # a dash for the CU column
-    # Otherwise, copy over the CU name
-    CU       = dplyr::if_else(Resolution == "SMU", "-", Name),
-    # If forecast info is blank, replace it with a space (instead of NA)
+    # Rename SMU column
+    SMU = smu_name,
+    # Create CU column: dash for SMU-level rows, else copy CU name
+    CU = dplyr::if_else(Resolution == "SMU", "-", Name),
+    # Replace NA in Forecast with blank for table clarity
     Forecast = dplyr::coalesce(Forecast, "")
   ) %>%
+  # Keep only the columns that appear in the slide table
   dplyr::select(smu_area, smu_species, SMU, Resolution, CU, Outlook, Forecast)
 
-################################################################################
-### SET THE ORDER OF AREAS/SPECIES
 
-# Dawn likes to present info in the following order (generally North -->)
+################################################################################
+### SET SLIDE ORDER
+################################################################################
+
+# Define the order of areas for presentation (North to South)
 area_order = c("YUKON TRANSBOUNDARY", "NORTH COAST", "SOUTH COAST", "FRASER AND INTERIOR")
 
-# This is the order for reporting species info
-### COME BACK TO THIS... I THINK LAKE TYPE VS RIVER & PINK / EVEN GOT ADJUSTED EARLIER
+# Define the order of species for presentation
+# Notes:
+# • Some species are lake type or river type sockeye
+# • Pink Even is not used for North Coast
 species_order = c(
   "Sockeye Lake Type",
   "Sockeye River Type",
-  "Sockeye",             # FRASER AND INTERIOR only
-  "Pink Even",           # not for NORTH COAST
-  "Pink",                # NORTH COAST
+  "Sockeye",
+  "Pink Even",
+  "Pink",
   "Chinook",
   "Coho",
   "Chum"
 )
 
-# Build a named list keyed by "AREA_SPECIES" and sort it
+# Split table into list by "AREA_SPECIES"
 keys = paste(tp_long$smu_area, tp_long$smu_species, sep = "_")
 table_list = split(tp_long[, c("SMU", "Resolution", "CU", "Outlook", "Forecast")], keys)
 
-
-# Reorder table_list by area and species
+# Reorder tables according to area and species preferences
 reorder_table_list = function(tbl) {
   nm_split = do.call(rbind, strsplit(names(tbl), "_"))
   df_order = data.frame(
@@ -97,24 +89,24 @@ reorder_table_list = function(tbl) {
     species = nm_split[,2],
     stringsAsFactors = FALSE
   )
+  # Rank areas and species
   df_order$area_rank = match(df_order$area, area_order)
   df_order$species_rank = match(df_order$species, species_order)
+  # Any species not in species_order get pushed to end
   df_order$species_rank[is.na(df_order$species_rank)] = max(df_order$species_rank, na.rm = TRUE) + 1
-  df_order = df_order %>%
-    arrange(area_rank, species_rank)
+  # Reorder table list
+  df_order = df_order %>% arrange(area_rank, species_rank)
   tbl[df_order$name]
 }
 table_list = reorder_table_list(table_list)
 
+
 ################################################################################
-### ADJUST STYLING
+### TABLE STYLING FUNCTIONS
+################################################################################
 
-## Table styling
-# Make the header dark grey/blue. The border surrounding the table should be the same.
-# The first column (SMU) should be dark grey, the rest a light grey
-# Centre all the text except what's in the first column
+# Style table headers, borders, background colors, and alignment
 style_table = function(ft) {
-
   ft = bg(ft, part = "header", bg = "#3B4A5A") %>%
     color(part = "header", color = "white") %>%
     bold(part = "header", bold = TRUE) %>%
@@ -128,9 +120,7 @@ style_table = function(ft) {
   return(ft)
 }
 
-## Adjust the font size based on the number of rows so all the text fits in the slide
-# If tables have many rows, they might extend beyond slide limits and need to be
-# manually fixed
+# Adjust font size depending on number of rows
 adjust_font = function(ft) {
   n_rows = nrow(ft$body$dataset)
   size = if (n_rows <= 5) 14 else if (n_rows <= 10) 10 else 8
@@ -139,32 +129,27 @@ adjust_font = function(ft) {
   return(ft)
 }
 
-################################################################################
-### READ IN POWERPOINT AND SET DIMENSIONS FOR ADDING THINGS
 
-# Read in the PowerPoint that has the intro slides and basic template
+################################################################################
+### READ IN POWERPOINT TEMPLATE AND SET DIMENSIONS
+################################################################################
+
+# Load the PPTX template with intro slides already in place
 ppt = read_pptx("draftPrelimPres.pptx")
 
-# Placeholder map - will need to be replaced with true map when ready
+# Map placeholder (will be replaced with final GIS map)
 fraser_map = "data/maps/fraserMap.png"
 
-# These are the names of the slide layouts e.g., if you go View --> Slide Master
-# This is the name of the slide with default layout view that gets filled in
-layout_name = "1_Title and Content"
-# This is the master slide. Needs to be there for reference for some reason, but
-# doesn't get adjusted
-master_name = "DFO-pp-template"
+# Slide layout names
+layout_name = "1_Title and Content"  # Layout for new slides
+master_name = "DFO-pp-template"      # Reference master slide
 
-
-# Get the slide dimensions
+# Slide dimensions
 sz = slide_size(ppt)
 slide_w = sz$width
 slide_h = sz$height
 
-# These are for setting the dimensions of the tables/margins on the sides etc
-# Were set through trial and error
-# Note that the table won't always match the height of the map... it depends on
-# how many rows of data there are
+# Layout positioning parameters (trial-and-error)
 left_margin  = 0.5
 right_margin = 0.5
 map_width    = 7.95
@@ -172,10 +157,11 @@ map_height   = 5.14
 table_width  = 4.2
 table_height = map_height
 
+# Vertical positions
 map_y   = (slide_h - map_height) / 2
 table_y = map_y
 table_x = left_margin
-map_x = slide_w - map_width - right_margin
+map_x   = slide_w - map_width - right_margin
 
 # Shift table down for species text box
 species_height = 0.4
@@ -184,16 +170,8 @@ table_y = table_y + species_height
 
 
 ################################################################################
-# Loop to add slides
-
-# It loops through each  species/area to generate a new slide
-#
-
-# This makes 1 slide for each Area/SMU. However, Fraser and Interior data, the
-# Chinook and Sockeye data are so detailed that 1 slide per SMU is created
-
-
-
+### LOOP THROUGH TABLES TO ADD SLIDES
+################################################################################
 
 for (nm in names(table_list)) {
   df_full = table_list[[nm]]
@@ -201,36 +179,53 @@ for (nm in names(table_list)) {
   area = area_species[1]
   species = area_species[2]
 
-  # Split FRASER AND INTERIOR Chinook/Sockeye by SMU
+  # SPECIAL CASE: Fraser/Interior Chinook or Sockeye → one slide per SMU
   if (area == "FRASER AND INTERIOR" & species %in% c("Chinook", "Sockeye")) {
     smus = unique(df_full$SMU)
     for (smu_val in smus) {
       df = df_full %>% filter(SMU == smu_val)
-      # create slide
+      # Create table for slide
       ft = flextable(df) %>% adjust_font()
       col_count = ncol(df)
       col_width = table_width / col_count
       ft = width(ft, j = 1:col_count, width = col_width)
 
+      # Add new slide
       ppt = add_slide(ppt, layout = layout_name, master = master_name)
-      ppt = ph_with(ppt, value = paste0("OUTLOOKS – ", area), location = ph_location_type(type = "title"))
-      ppt = ph_with(ppt, value = ft, location = ph_location(left = table_x, top = table_y, width = table_width, height = table_height))
 
+      # Add banner at top of slide: "OUTLOOKS – AREA NAME"
+      ppt = ph_with(ppt, value = paste0("OUTLOOKS – ", area),
+                    location = ph_location_type(type = "title"))
+
+      # Add table
+      ppt = ph_with(ppt, value = ft,
+                    location = ph_location(left = table_x, top = table_y,
+                                           width = table_width, height = table_height))
+
+      # Add subtitle above table: "Area – Species"
+      # Note: This was previously only species; now includes area first
+      # Convert area to title case for subtitle bove table
+      area_title = tools::toTitleCase(tolower(area))
+      subtitle_text = paste0(area_title, " – ", species)
       species_text = fpar(
-        ftext(species, prop = fp_text(color = "black", font.size = 20, font.family = "Segoe UI Semibold", bold = TRUE)),
+        ftext(subtitle_text,
+              prop = fp_text(color = "black", font.size = 20,
+                             font.family = "Segoe UI Semibold", bold = TRUE)),
         fp_p = fp_par(text.align = "left")
       )
-      ppt = ph_with(ppt, value = species_text, location = ph_location(left = table_x, top = species_y, width = table_width, height = species_height))
+      ppt = ph_with(ppt, value = species_text,
+                    location = ph_location(left = table_x, top = species_y,
+                                           width = table_width, height = species_height))
 
+      # Add map if exists
       if (file.exists(fraser_map)) {
-        ppt = ph_with(
-          ppt,
-          value = external_img(fraser_map, width = map_width, height = map_height),
-          location = ph_location(left = map_x, top = map_y, width = map_width, height = map_height)
-        )
+        ppt = ph_with(ppt,
+                      value = external_img(fraser_map, width = map_width, height = map_height),
+                      location = ph_location(left = map_x, top = map_y,
+                                             width = map_width, height = map_height))
       }
 
-      # Add notes
+      # Add slide notes (narrative text)
       if (all(c("smu_area", "smu_species", "Narrative") %in% names(tabPrep))) {
         narratives_df = tabPrep %>%
           filter(trimws(smu_area) == trimws(area),
@@ -247,34 +242,55 @@ for (nm in names(table_list)) {
         ppt = set_notes(ppt, value = notes_text, location = notes_location_type("body"))
       }
     }
+
   } else {
 
-    # Regular slide for all other species/areas
+    # REGULAR SLIDE: All other species/areas (not Fraser/Interior Chinook/Sockeye)
+    # ----------------------------
+    # df_full contains all SMUs/CUs for this area/species
     df = df_full
+
+    # Create table for slide and adjust font
     ft = flextable(df) %>% adjust_font()
     col_count = ncol(df)
     col_width = table_width / col_count
     ft = width(ft, j = 1:col_count, width = col_width)
 
+    # Add new slide
     ppt = add_slide(ppt, layout = layout_name, master = master_name)
-    ppt = ph_with(ppt, value = paste0("OUTLOOKS – ", area), location = ph_location_type(type = "title"))
-    ppt = ph_with(ppt, value = ft, location = ph_location(left = table_x, top = table_y, width = table_width, height = table_height))
 
+    # Add banner at top: "OUTLOOKS – AREA NAME"
+    ppt = ph_with(ppt, value = paste0("OUTLOOKS – ", area),
+                  location = ph_location_type(type = "title"))
+
+    # Add table
+    ppt = ph_with(ppt, value = ft,
+                  location = ph_location(left = table_x, top = table_y,
+                                         width = table_width, height = table_height))
+
+    # Add subtitle above table: "Area – Species"
+    # This replaces the old behavior where only species was shown
+    area_title = tools::toTitleCase(tolower(area))
+    subtitle_text = paste0(area_title, " – ", species)
     species_text = fpar(
-      ftext(species, prop = fp_text(color = "black", font.size = 20, font.family = "Segoe UI Semibold", bold = TRUE)),
+      ftext(subtitle_text,
+            prop = fp_text(color = "black", font.size = 20,
+                           font.family = "Segoe UI Semibold", bold = TRUE)),
       fp_p = fp_par(text.align = "left")
     )
-    ppt = ph_with(ppt, value = species_text, location = ph_location(left = table_x, top = species_y, width = table_width, height = species_height))
+    ppt = ph_with(ppt, value = species_text,
+                  location = ph_location(left = table_x, top = species_y,
+                                         width = table_width, height = species_height))
 
+    # Add map if available
     if (file.exists(fraser_map)) {
-      ppt = ph_with(
-        ppt,
-        value = external_img(fraser_map, width = map_width, height = map_height),
-        location = ph_location(left = map_x, top = map_y, width = map_width, height = map_height)
-      )
+      ppt = ph_with(ppt,
+                    value = external_img(fraser_map, width = map_width, height = map_height),
+                    location = ph_location(left = map_x, top = map_y,
+                                           width = map_width, height = map_height))
     }
 
-    # Add notes
+    # Add slide notes (narrative text)
     if (all(c("smu_area", "smu_species", "Narrative") %in% names(tabPrep))) {
       narratives_df = tabPrep %>%
         filter(trimws(smu_area) == trimws(area),
@@ -292,7 +308,9 @@ for (nm in names(table_list)) {
   }
 }
 
+
 ################################################################################
 ### SAVE PRESENTATION
+################################################################################
 
 print(ppt, target = "updated_presentation.pptx")
